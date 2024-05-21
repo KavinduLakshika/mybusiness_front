b65bb445ac8e60d94724f081dc424938e4037d77
Uditha Bandara <udithabandara9@gmail.com>
2024-05-21 05:34:27 +0530

    sixth commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 import NavBar from "./NavBar";
 import { ReactNode, useState } from 'react';
-// import 'animate.css';
+import 'animate.css';
 import { Link } from "react-router-dom";
 
 interface Props {
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-// import 'animate.css';
+import 'animate.css';
 import {ReactNode} from 'react';
 
 interface Props {
