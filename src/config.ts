cb637dd617ab881f0f587f01edb6e8cbd36b0b52
KavinduLakshika <lakshikasenarath123@gmail.com>
2025-06-14 11:00:36 +0530

    Config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const config = {
   BASE_URL: "https://mybussinessbackend.etherealstudios.website",
+  //  BASE_URL: "https://mybusiness-back.onrender.com",
   // BASE_URL: "http://localhost:3000",
 };
 
